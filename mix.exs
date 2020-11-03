563fd502f3e930f62b8fe651620df5a277c51255
zhanglinjie <zhanglinjie@big.one>
2020-11-03 15:23:47 +0800

    doc: update the mix.exs

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule BonyWs.MixProject do
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      description: "A simple websocket client."
     ]
   end
 
@@ -18,10 +20,18 @@ defmodule BonyWs.MixProject do
     ]
   end
 
+  defp package() do
+    [
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/ljzn/bony_ws"}
+    ]
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:jason, "~> 1.2.2"}
+      {:jason, "~> 1.2.2"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
